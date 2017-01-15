bdce0d480fc334ea03725c988b997591364b125c
lambert pouguem wassi <polawassi@yahoo.com>
2017-01-15 04:25:16 +0000

    Fix create, add flash and erros message, create show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
   get 'about',to: 'pages#about'
+  
+  resources :articles 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
